1c57592bf93cfdb260f7bff072de8f9b9c234e56
Sean Elliott <seanmtelliott@gmail.com>
2024-11-01 10:20:55 -0400

    added misspecification doc

--- code/scripts/identification/identification_sims.py
+++ code/scripts/identification/identification_sims.py
@@ -21,9 +21,9 @@ size = 1000
 dist = "lognormal"
 
 means = []
-for i in range(99):
-    revenue_params = {'a':1,'b':1,'c':1,'n':1,'m':1,'cons':2} # where F(k,s) = a*k**n + b*s**m + c*k*s
-    dist_params = {'mean_k': 0.75,'mean_s': 0.5,'variance_k': 2,'variance_s':2,'correlation': 0}
+for i in range(10):
+    revenue_params = {'a':1,'b':1,'c':0.75,'n':1,'m':1,'cons':2} # where F(k,s) = a*k**n + b*s**m + c*k*s
+    dist_params = {'mean_k': 1,'mean_s': 0.5,'variance_k': 2,'variance_s':2,'correlation': 0}
     sim_results = rmm.model_sim(size,dist,dist_params,revenue_params,tolerance=0.005)
     est_wages = rmm.get_estimated_moments(sim_results)
     means.append(est_wages)
@@ -34,8 +34,8 @@ mean_s = stats.mean(means_df['s'])
 
 
 # Distribution of the means
-plt.hist(means_df['k'],bins=10, color='red', edgecolor='black')
+plt.hist(means_df['k'],bins=1, color='red', edgecolor='black')
 plt.axvline(mean_s, color='k', linestyle='dashed', linewidth=1)
 
-plt.hist(means_df['s'],bins=10, color='skyblue', edgecolor='black')
+plt.hist(means_df['s'],bins=1, color='skyblue', edgecolor='black')
 plt.axvline(mean_k, color='k', linestyle='dashed', linewidth=1)
