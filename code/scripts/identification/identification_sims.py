ebefe78c674a057f6f024a27cf765b96bc1201e4
Sean Elliott <seanmtelliott@gmail.com>
2024-11-14 18:12:25 -0500

    a bit of work on the simulations

--- code/scripts/identification/identification_sims.py
+++ code/scripts/identification/identification_sims.py
@@ -22,8 +22,8 @@ dist = "lognormal"
 
 means = []
 for i in range(25):
-    revenue_params = {'a':1,'b':1,'c':0.5,'n':1,'m':1,'cons':0} # where F(k,s) = a*k**n + b*s**m + c*k*s
-    dist_params = {'mean_k': 0.5,'mean_s': 0,'variance_k': 1,'variance_s':1,'correlation': 0}
+    revenue_params = {'a':1,'b':1,'c':0.001,'n':1,'m':1,'cons':3} # where F(k,s) = a*k**n + b*s**m + c*k*s
+    dist_params = {'mean_k': 0.25, 'mean_s': 0,'variance_k': 1,'variance_s': 1,'correlation': 0}
     sim_results = rmm.model_sim(size,dist,dist_params,revenue_params,tolerance=0.01)
     est_wages = rmm.get_estimated_moments(sim_results)
     means.append(est_wages)
